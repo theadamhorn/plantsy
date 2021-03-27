663a38a51b862a6b77a77730ef901ad56c56be0b
Leslie Williams <71472699+Sesdesoir@users.noreply.github.com>
2021-03-27 11:58:08 -0700

    Update Plants.js

--- models/Plants.js
+++ models/Plants.js
@@ -4,6 +4,24 @@ const sequelize = require('../config/connection');
 class Plants extends Model {}
 
 Plants.init({
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      plant_name:{
+        type: DataTypes.STRING,
+        allowNull: false,
 
-}
+    },
+},
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'Plants',
+    }
 );
+module.exports = Plants;
