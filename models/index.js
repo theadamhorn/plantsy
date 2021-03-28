2a543cd6e04dac2b109b41e5e1041e886d75d89b
Skyler Blakeslee <skylerblakeslee@gmail.com>
2021-03-28 14:34:38 -0700

    added something to models index

--- models/index.js
+++ models/index.js
@@ -2,4 +2,17 @@ const Plants = require('./Plants');
 const Users = require('./Users');
 const Owned_Plants = require('./Owned_Plants');
 
-module.exports = { Users, Plants, Owned_Plants};
+// Not sure if this is correct
+
+Users.hasMany(Owned_Plants, {
+    foreignKey: 'user_id',
+});
+
+Owned_Plants.belongsTo(Users, {
+    foreignKey: 'user_id',
+});
+
+
+
+module.exports = { Users, Plants, Owned_Plants };
+
