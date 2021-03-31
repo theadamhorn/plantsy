5a6b8a39cef4975a0f07140038deab5bffb3d7a3
sloth740 <71675077+sloth740@users.noreply.github.com>
2021-03-30 21:13:47 -0700

    updated routes

--- controllers/api/plantsRoutes.js
+++ controllers/api/plantsRoutes.js
@@ -2,7 +2,66 @@ const router = require('express').Router();
 const { Plants } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.post('/', withAuth, async (req, res) => {
+// Get all plants from global list
+router.get('/plants', async (req, res) => {
+    // find all plants
+    try {
+        const plantsData = await Plants.findAll();
+        res.status(200).json(plantsData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+
+});
+
+// get one plant from global plant list
+router.get('/plants/:id', async (req, res) => {
+    // find a single product by its `id`
+    // be sure to include its associated Category and Tag data
+    try {
+        const plantsData = await Plants.findByPk(req.params.id, {
+            include: [{ model: Category }, { model: Tag }]
+        });
+        if (!plantsData) {
+            res.status(404).json({ message: 'No plant found.' });
+            return;
+        }
+        res.status(200).json(plantsData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+// update given plant in global plant list
+router.put('/plants/:id', withAuth, (req, res) => {
+    Plants.update(
+        {
+            genus: req.body.genus,
+            species: req.body.species,
+            variety: req.body.variety,
+            common_name: req.body.common_name,
+            watering: req.body.watering,
+            temperature: req.body.temperature,
+            humidity: req.body.humidity,
+            light: req.body.light,
+            description: req.body.description,
+        },
+        {
+            where: {
+                id: req.params.id,
+            },
+        }
+    )
+        .then((updatedPlants) => {
+            res.json(updatedPlants);
+        })
+        .catch((err) => res.json(err));
+});
+
+
+// Create a plant to the global list
+router.post('/plants', withAuth, async (req, res) => {
     try {
         const newPlants = await Plants.create({
             ...req.body,
@@ -15,7 +74,8 @@ router.post('/', withAuth, async (req, res) => {
     }
 });
 
-router.delete('/:id', withAuth, async (req, res) => {
+// Delete a plant from the global list
+router.delete('/plants/:id', withAuth, async (req, res) => {
     try {
         const plantsData = await Plants.destroy({
             where: {
