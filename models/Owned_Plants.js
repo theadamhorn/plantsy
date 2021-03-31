d1f232871604821cd74ddc4b023e9b7b1b927cc8
Leslie Williams <71472699+Sesdesoir@users.noreply.github.com>
2021-03-30 19:48:07 -0700

    Update Owned_Plants.js

--- models/Owned_Plants.js
+++ models/Owned_Plants.js
@@ -13,6 +13,10 @@ Owned_Plants.init({
     user_id: {
         type: DataTypes.INTEGER,
         allowNull: false,
+            references: {
+                model: 'Users',
+                key: 'id'
+            }
       },
       species:{
         type: DataTypes.STRING,
