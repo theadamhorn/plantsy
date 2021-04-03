670202b274fb6e80a4d9cf0c5b4c7b6f517ef2b5
Leslie Williams <71472699+Sesdesoir@users.noreply.github.com>
2021-04-03 10:07:36 -0700

    Update index.js

--- models/index.js
+++ models/index.js
@@ -2,17 +2,13 @@ const Plants = require('./Plants');
 const Users = require('./Users');
 const Owned_Plants = require('./Owned_Plants');
 
-// Not sure if this is correct
-
 Users.hasMany(Owned_Plants, {
-    foreignKey: 'user_id',
+    foreignKey: user_id
 });
 
 Owned_Plants.belongsTo(Users, {
-    foreignKey: 'user_id',
+    foreignKey: user_id
 });
 
-
-
-module.exports = { Users, Plants, Owned_Plants };
+module.exports = { Users, Plants, Owned_Plants};
 
