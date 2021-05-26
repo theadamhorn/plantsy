89655d9cea8e68e0b6f171c6f65783cc5a12042a
Kiel Sprague <aereisdin@gmail.com>
2021-05-26 12:02:43 -0700

    accordion for user done

--- client/src/components/Accordion.js
+++ client/src/components/Accordion.js
@@ -9,22 +9,24 @@ import { faSeedling } from '@fortawesome/free-solid-svg-icons'
 export default function Accordion(props) {
     var [Gardener, setGardener] = useState(false);
     var [Plant, setPlant] = useState(false);
+    var [OwnedPlant, setOwnedPlant] = useState(false);
 
     useEffect(() => {
         var location = document.location.pathname;
 
         if (location === '/plants') {
             setPlant(true);
-
-            // API call depending on the page, set results into hook, then pass hook into {Accordion}
         }
         if (location === '/gardeners') {
             setGardener(true)
         }
+        if (location === '/profile') {
+            setOwnedPlant(true)
+        }
 
     }, [props])
 
-
+    var ownedPlants = props.OwnedPlants
     var plants = props.plants;
     var users = props.users;
 
@@ -124,6 +126,58 @@ export default function Accordion(props) {
                                     <div className="accordion-body">
                                         A users garden button will go here.
                         </div>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>)
+    }
+
+    else if (OwnedPlant === true) {
+        return (
+            <div>
+                <div className="accordion">
+                    {ownedPlants.map(plant => {
+                        return (
+                            <div className="accordion-item" key={plant.id}>
+                                <h2 className="accordion-header" id={"heading" + plant.id}>
+                                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={"#collapse" + plant.id} aria-expanded="true" aria-controls={"collapse" + plant.id}>
+                                        {plant.genus} {plant.species} --- {plant.common_name}
+
+                                    </button>
+                                </h2>
+                                <div id={"collapse" + plant.id} className="accordion-collapse collapse" aria-labelledby={"heading" + plant.id} data-bs-parent={"#heading" + plant.id}>
+                                    <div className="accordion-body">
+                                        <div className="row justify-content-evenly">
+                                            <PlantModal
+                                                id={plant.id}
+                                                genus={plant.genus}
+                                                species={plant.species}
+                                                variety={plant.variety}
+                                                common_name={plant.common_name}
+                                                photo={plant.photo}
+                                                watering={plant.watering}
+                                                temperature={plant.temperature}
+                                                humidity={plant.humidity}
+                                                light={plant.light}
+                                                description={plant.description}
+                                                />
+                                            <PlantBadge
+                                                watering={plant.watering}
+                                                temperature={plant.temperature}
+                                                humidity={plant.humidity}
+                                                light={plant.light}
+                                            />
+                                            <div className="col-auto plant">
+                                                <button type="button" className="plant_button" data-bs-toggle="modal" data-bs-target={"#plantModal" + plant.id}>
+                                                <p><FontAwesomeIcon icon={faSeedling} className="fa-2x" /></p>
+                                                    <p>Plant</p>
+                                                    <p>Profile</p>
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         )
