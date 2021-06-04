fc7a05622b75392c8989348a9789b42713e136a4
Kiel Sprague <aereisdin@gmail.com>
2021-06-03 21:05:18 -0700

    Footer link for EE

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 export default function Footer() {
     return (
         <footer className="footer text-center pt-3">
-            <p>&copy; Plantsy 2021 ~ Grow up!</p>
+            <p>&copy; Plantsy 2021 ~<Link to="./thanks"> Grow up!</Link></p>
         </footer>
     )
 }
