06ec349ffc93480378942df339b9f6178ace0831
sloth740 <71675077+sloth740@users.noreply.github.com>
2021-06-05 12:40:43 -0700

    pushing to heroku

--- server.js
+++ server.js
@@ -11,13 +11,17 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static('client/build'));
-// }
+app.use(routes);
 
-// app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-// });
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
+
+
 
 const sess = {
     secret: 'Super secret secret',
@@ -37,7 +41,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(compression());
 
-app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
